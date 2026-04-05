1ef54bb9e0cc5eebc8295b8f1008002201b9d80b
Wooden Chair <hua.zheng@embeddedboys.com>
2026-04-05 21:16:15 +0800

    Add site configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://embeddedboys.github.io',
 	base: '/Pico_DM_1P5623',
 	integrations: [
 		starlight({
